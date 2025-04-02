115ac5b29f60c6b922dce1e704b440e17fd93e4c
nicolas-liima <nicolastoledolima18@icloud.com>
2025-04-02 08:06:04 -0300

    REFACTOR: Adicionando metodo de criação do pdf no teste

--- TESTE2/src/tests/ExtractorTests.java
+++ TESTE2/src/tests/ExtractorTests.java
@@ -2,23 +2,44 @@ package tests;
 
 import extractor.ExtractorPdf;
 import org.junit.jupiter.api.Test;
+import scraper.downloadPdf.DownloadPdf;
+
 import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class ExtractorTests {
 
     @Test
     public void testExtractTextFromPdf() {
+        DownloadPdf downloadPdf = new DownloadPdf();
+        Path downloadFolder = Paths.get("src\\downloads");
+
+            downloadPdf.downloadPdf(
+                    "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf",
+                    downloadFolder.resolve("Anexo_I.pdf").toString()
+            );
+
+
         String pdfPath = "src\\downloads\\Anexo_I.pdf";
 
-        File file = new File(pdfPath);
-        assertTrue(file.exists(), "O arquivo PDF não foi encontrado!");
 
-        String extractedText = ExtractorPdf.extractTextFromPdf(pdfPath);
+        try {
+            File file = new File(pdfPath);
+            assertTrue(file.exists(), "O arquivo PDF de teste não foi encontrado!");
 
-        assertTrue(extractedText.contains("Rol de Procedimentos"), "O texto extraído deve conter 'Rol de Procedimentos'");
+            String extractedText = ExtractorPdf.extractTextFromPdf(pdfPath);
+            assertTrue(extractedText.contains("Texto esperado no PDF"), "O PDF não contém o texto esperado.");
+
+        }catch(Exception e) {
+            System.out.println("Erro ao ler o arquivo PDF!" + e.getMessage());
+        }
+
     }
+
+
+
 }
-
-
